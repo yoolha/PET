05ac833ce7ab5112333a6f0bae16219c134479a9
yoolha <dbstjwls0247@naver.com>
2025-06-16 22:56:23 +0900

    feat: socket 세팅

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
 const port = 3000;
 
+const registerSocketHandlers = require('./sockets/socketHandler');
 const dnsRouter = require('./routes/dnsRoute');
 
+app.use(cors());
 app.use(express.json());
 app.use('/dns', dnsRouter);
 
@@ -11,6 +18,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+const io = new Server(server, {
+  cors: { origin: '*', methods: ['GET', 'POST'] },
+});
+
+registerSocketHandlers(io);
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
