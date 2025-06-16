47455af43d8fe97f281898a177a4027b3a1037e1
yoolha <dbstjwls0247@naver.com>
2025-06-16 23:21:19 +0900

    feat: DNS CNAME 구현

--- server/routes/dnsRoute.js
+++ server/routes/dnsRoute.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const { resolveCNAME } = require('../services/dnsService');
+const { resolveCNAME, resolveMX } = require('../services/dnsService');
 
 router.post('/cname', async (req, res) => {
   const { domain } = req.body;
   try {
     const result = await resolveCNAME(domain);
     res.json({ type: 'CNAME', result });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+router.post('/mx', async (req, res) => {
+  const { domain } = req.body;
+  try {
+    const result = await resolveMX(domain);
+    res.json({ type: 'MX', result });
   } catch (err) {
     res.status(500).json({ error: err });
   }
